f93f31e40d4e19f1ad29d2bd040daa51c0809f4f
BrandonT <69981796+BrandonT310442@users.noreply.github.com>
2025-01-18 01:15:33 -0500

    server and frontend register

--- qnet/src/pages/Register.vue
+++ qnet/src/pages/Register.vue
@@ -4,11 +4,11 @@
       <div class="bottom"></div>
       <div class="center">
         <h2>Sign Up</h2>
-        <div>
+        <div id="input">
           <label for="email">Email</label>
           <input v-model="email" type="email" placeholder="e.g. John Smith" name="email" />
         </div>
-        <div>
+        <div id="input">
         <br>
         <br>
           <label for="password">Password</label>
@@ -56,29 +56,30 @@
     }
   }
   </script>
-  
   <style scoped lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
-  
+
   *, *:before, *:after {
     box-sizing: border-box;
     margin-left:-10px;
     margin-top:-10px;
   }
-  
+  #input{
+    width: 300px !important;
+  }
   body {
     min-height: 100vh;
     font-family: 'Raleway', sans-serif;
     margin: 0;
     padding: 0;
   }
-  
+
   .container {
     position: absolute;
     width: 100%;
     height: 100%;
     overflow: hidden;
-  
+
     &:hover, &:active {
       .top, .bottom {
         &:before, &:after {
@@ -87,14 +88,14 @@
           transition-delay: 0s;
         }
       }
-  
+
       .center {
         opacity: 1;
         transition-delay: 0.2s;
       }
     }
   }
-  
+
   .top, .bottom {
     &:before, &:after {
       content: '';
@@ -112,29 +113,29 @@
       transition-delay: 0.2s;
     }
   }
-  
+
   .top {
     &:before {
       transform: rotate(45deg);
-      background: #e46569;
+      background: #FF6C64; /* Coral Red */
     }
     &:after {
       transform: rotate(135deg);
-      background: #ecaf81;
+      background: #FFB79A; /* Soft Peach */
     }
   }
-  
+
   .bottom {
     &:before {
       transform: rotate(-45deg);
-      background: #60b8d4;
+      background: #B3A0D1; /* Lavender */
     }
     &:after {
       transform: rotate(-135deg);
-      background: #3745b5;
+      background: #A7E6D7; /* Soft Mint */
     }
   }
-  
+
   .center {
     position: absolute;
     width: 400px;
@@ -152,11 +153,11 @@
     transition: all 0.5s cubic-bezier(0.445, 0.05, 0, 1);
     transition-delay: 0s;
     color: #333;
-  
+
     h2 {
       margin: 0 0 20px 0;
     }
-  
+
     input {
       width: 100%;
       padding: 15px;
@@ -165,21 +166,20 @@
       border: 1px solid #ccc;
       font-family: inherit;
     }
-  
+
     button {
       padding: 15px;
-      background-color: #60b8d4;
+      background-color: #B3A0D1; /* Lavender */
       border: none;
       color: white;
       font-size: 16px;
       cursor: pointer;
       border-radius: 5px;
       margin-top: 15px;
-  
+
       &:hover {
-        background-color: #3745b5;
+        background-color: #A7E6D7; /* Soft Mint */
       }
     }
   }
-  </style>
-  
+</style>
